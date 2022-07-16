5ac75aeb5157727c932fd3aed635d2e3571bb9a8
Ada W <adabingw@yahoo.com>
2022-07-16 17:21:24 -0600

    yare yare

--- onono/src/About/About.js
+++ onono/src/About/About.js
@@ -1,11 +1,27 @@
 import './About.css'
+import arrrex from './arrrex.jpg'
+import blingblingda from './blingblingda.jpg'
 
 function About() {
     return (
         <div className="About">
-          <h1>About Uth!</h1>
-          <h3>Arrrex</h3>
-          <h3>blingblingda</h3>
+          <h1 className="abouth1">About Uth!</h1>
+
+          <div className="arrrex-flex">
+            <div>
+                <h3 className="abouth3">Arrrex</h3>
+                <p className="aboutp">INCOMING!!! sophomore at the University of Waterloo <br/> I like cats n stuff. </p>
+            </div>
+            <img src={arrrex} width="300" height="300" alt="arrrex" className="img arrrex-pic"/>
+          </div>
+
+          <div className="blingblingda-flex">
+            <img src={blingblingda} width="300" height="300" alt="arrrex" className="img"/>
+            <div>
+                <h3 className="abouth3">blingblingda</h3>
+                <p className="aboutp">ada likes bts and levi ackerman.</p>
+            </div>
+          </div>
         </div>
       );
 }
--- onono/src/Play/Tile.js
+++ onono/src/Play/Tile.js
@@ -1,3 +1,15 @@
 /**
  * Tiles components
  */
+
+import './Tile.css'
+
+function Tile() {
+    return (
+         <div>
+            <p>yes.</p>
+         </div>
+    );
+ }
+ 
+ export default Tile
