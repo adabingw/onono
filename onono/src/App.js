56efb37dbc0173254604326d3eda4d865f582d1b
Alex S <qwshao@uwaterloo.ca>
2022-07-16 16:11:10 -0700

    routing

--- onono/src/App.js
+++ onono/src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
-import StartScreen from './StartScreen/StartScreen.js'
-import About from './About/About.js'
-import { useState } from 'react'
+import React from 'react';
+import StartScreen from './Pages/StartScreen';
+import About from './Pages/About';
+import ErrorPage from './Pages/ErrorPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-
-  const [active,setActive] = useState(0)
+function App() {  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <StartScreen />
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/"> StartScreen </Link>
+        <Link to="/about"> About </Link>
+        <Link to="*"> Error </Link>
+      </nav>
+      <Routes>
+        <Route exact path="/" element={<StartScreen />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ onono/src/Pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>Error</h1>
+      <h3>404</h3>
+      <h3>Page not found</h3>
+    </div>
+  );
+}
+
+export default ErrorPage;
